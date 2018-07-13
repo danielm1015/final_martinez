a1502692e55b37ad54bd1bff4b0f68ce9273447b
danielm1015 <daniel.martinez1015@gmail.com>
2018-07-13 12:46:04 -0700

    inital commt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ShoppingCartPage } from '../pages/shopping-cart/shopping-cart';
+import { TabsPage } from '../tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ShoppingCartPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ShoppingCartPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
---
+++ src/tabs/tabs.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { HomePage } from "../pages/home/home";
+import { ShoppingCartPage } from "../pages/shopping-cart/shopping-cart";
+
+@Component({
+    selector: 'page-tabs',
+    template: `
+        <ion-tabs>
+        <ion-tab [root]="homePage" tabTitle="Home" tabIcon="home"></ion-tab>
+        <ion-tab [root]="shoppingCartPage" tabTitle="Cart" tabIcon="star"></ion-tab>
+        </ion-tabs>
+    `
+})
+
+export class TabsPage {
+    homePage = HomePage;
+    shoppingCartPage = ShoppingCartPage;
+}
